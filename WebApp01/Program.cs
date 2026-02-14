703143700b1d81b0a2839e0f5951154ddf69761f
Egutierrez <edder.gutierrez@yahoo.com>
2026-02-13 19:53:05 -0600

    e-spec now with dev and prod

--- WebApp01/Program.cs
+++ WebApp01/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello GitHUb Actions!    e-spec is live with latest code changes.");
 
 app.Run();
